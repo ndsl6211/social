5f050a6b29f124c02ae6acf2ed5b98142fdf5034
Mashu <ndsl6211@gmail.com>
2022-04-07 11:00:54 +0800

    add usecase for login

--- go.mod
+++ go.mod
@@ -14,8 +14,11 @@ require (
 )
 
 require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
--- internal/usecase/user/register/register.go
+++ internal/usecase/user/register/register.go
@@ -1,7 +1,11 @@
 package register
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
 	"mashu.example/internal/entity"
 	"mashu.example/internal/usecase"
 	"mashu.example/internal/usecase/repository"
@@ -33,7 +37,9 @@ func (uc *RegisterUseCase) Execute() {
 	)
 
 	if err := uc.userRepo.Save(user); err != nil {
-		uc.res.Err = err
+		errMsg := fmt.Sprintf("user %s already exist", uc.req.username)
+		logrus.Info(errMsg)
+		uc.res.Err = errors.New(errMsg)
 		return
 	}
 }
@@ -46,8 +52,12 @@ func NewRegisterUseCase(
 	return &RegisterUseCase{userRepo, req, res}
 }
 
-func NewRegisterUseCaseReq() *RegisterUseCaseReq {
-	return &RegisterUseCaseReq{}
+func NewRegisterUseCaseReq(
+	username string,
+	displayName string,
+	email string,
+) *RegisterUseCaseReq {
+	return &RegisterUseCaseReq{username, displayName, email}
 }
 
 func NewRegisterUseCaseRes() *RegisterUseCaseRes {
