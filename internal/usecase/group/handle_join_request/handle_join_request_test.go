3b5ebaa8cb87a3bf7023938d69304c65e87ccce7
winnie <winnie@winnie.com>
2022-04-13 00:23:31 +0800

    feat: edit handle_join_request_test

--- internal/usecase/group/handle_join_request/handle_join_request_test.go
+++ internal/usecase/group/handle_join_request/handle_join_request_test.go
@@ -80,5 +80,164 @@ func TestAcceptJoinRequest(t *testing.T) {
 
 	assert.Equal(t, len(group.JoinRequests), 0)
 	assert.Equal(t, len(group.Members), 1)
-	assert.Equal(t, group.Owner, approver)
+	assert.Equal(t, group.Owner.ID, approver.ID)
+}
+
+func TestRejectJoinRequest(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	requesterId := uuid.New()
+	groupId := uuid.New()
+	approverId := uuid.New()
+	ownerId := uuid.New()
+
+	owner := entity.NewUser(
+		ownerId,
+		"owner",
+		"owner display name",
+		"owner@email.com",
+		false,
+	)
+
+	requester := entity.NewUser(
+		requesterId,
+		"requester",
+		"requester display name",
+		"requester@email.com",
+		false,
+	)
+
+	approver := entity.NewUser(
+		approverId,
+		"approver",
+		"approver display name",
+		"approver@email.com",
+		false,
+	)
+
+	group := &entity.Group{
+		ID:         groupId,
+		Name:       "second group",
+		Owner:      owner,
+		Permission: group_permission.UNPUBLIC,
+		Admins:     []uuid.UUID{approverId},
+		CreatedAt:  time.Time{},
+		Members:    nil,
+		JoinRequests: []*entity.JoinRequest{
+			{
+				Requester: requesterId,
+				Group:     groupId,
+			},
+		},
+		InviteRequests: nil,
+	}
+
+	userRepo := mock.NewMockUserRepo(mockCtrl)
+	userRepo.EXPECT().GetUserById(requesterId).Return(requester, nil)
+	userRepo.EXPECT().GetUserById(approverId).Return(approver, nil)
+	groupRepo := mock.NewMockGroupRepo(mockCtrl)
+	groupRepo.EXPECT().GetGroupById(groupId).Return(group, nil)
+
+	groupRepo.EXPECT().Save(gomock.AssignableToTypeOf(&entity.Group{})).Do(
+		func(arg *entity.Group) { group = arg },
+	)
+
+	req := handle_join_request.NewHandleJoinRequestUseCaseReq(
+		requesterId,
+		groupId,
+		handle_join_request.REJECT_JOIN_REQUEST,
+		approverId,
+	)
+
+	res := handle_join_request.NewHandleJoinRequestUseCaseRes()
+	gc := handle_join_request.NewHandleJoinRequestUseCase(userRepo, groupRepo, &req, &res)
+	gc.Execute()
+
+	if res.Err != nil {
+		t.Errorf("failed to execute usecase")
+	}
+
+	assert.Equal(t, len(group.JoinRequests), 0)
+	assert.Equal(t, len(group.Members), 0)
+	assert.Equal(t, group.Owner.ID, owner.ID)
+}
+
+func TestDenyJoinRequest(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	requesterId := uuid.New()
+	groupId := uuid.New()
+	approverId := uuid.New()
+	ownerId := uuid.New()
+
+	owner := entity.NewUser(
+		ownerId,
+		"owner",
+		"owner display name",
+		"owner@email.com",
+		false,
+	)
+
+	requester := entity.NewUser(
+		requesterId,
+		"requester",
+		"requester display name",
+		"requester@email.com",
+		false,
+	)
+
+	approver := entity.NewUser(
+		approverId,
+		"approver",
+		"approver display name",
+		"approver@email.com",
+		false,
+	)
+
+	group := &entity.Group{
+		ID:         groupId,
+		Name:       "second group",
+		Owner:      owner,
+		Permission: group_permission.UNPUBLIC,
+		Admins:     nil,
+		CreatedAt:  time.Time{},
+		Members:    nil,
+		JoinRequests: []*entity.JoinRequest{
+			{
+				Requester: requesterId,
+				Group:     groupId,
+			},
+		},
+		InviteRequests: nil,
+	}
+
+	userRepo := mock.NewMockUserRepo(mockCtrl)
+	userRepo.EXPECT().GetUserById(requesterId).Return(requester, nil)
+	userRepo.EXPECT().GetUserById(approverId).Return(approver, nil)
+	groupRepo := mock.NewMockGroupRepo(mockCtrl)
+	groupRepo.EXPECT().GetGroupById(groupId).Return(group, nil)
+
+	groupRepo.EXPECT().Save(gomock.AssignableToTypeOf(&entity.Group{})).Do(
+		func(arg *entity.Group) { group = arg },
+	)
+
+	req := handle_join_request.NewHandleJoinRequestUseCaseReq(
+		requesterId,
+		groupId,
+		handle_join_request.ACCEPT_JOIN_REQUEST,
+		approverId,
+	)
+
+	res := handle_join_request.NewHandleJoinRequestUseCaseRes()
+	gc := handle_join_request.NewHandleJoinRequestUseCase(userRepo, groupRepo, &req, &res)
+	gc.Execute()
+
+	if res.Err != nil {
+		t.Errorf("failed to execute usecase")
+	}
+
+	assert.Equal(t, len(group.JoinRequests), 1)
+	assert.Equal(t, len(group.Members), 0)
 }
