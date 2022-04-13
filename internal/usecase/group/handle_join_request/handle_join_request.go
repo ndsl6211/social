6724868d34d641809dd0d0537a704d88323783dd
winnie <winnie@winnie.com>
2022-04-14 00:01:59 +0800

    feat: add add_admin

--- internal/usecase/group/handle_join_request/handle_join_request.go
+++ internal/usecase/group/handle_join_request/handle_join_request.go
@@ -30,42 +30,42 @@ type HandleJoinRequestUseCaseRes struct {
 type HandleJoinRequestUseCase struct {
 	userRepo  repository.UserRepo
 	groupRepo repository.GroupRepo
-	Req       *HandleJoinRequestUseCaseReq
-	Res       *HandleJoinRequestUseCaseRes
+	req       *HandleJoinRequestUseCaseReq
+	res       *HandleJoinRequestUseCaseRes
 }
 
 func (gc *HandleJoinRequestUseCase) Execute() {
-	requester, err := gc.userRepo.GetUserById(gc.Req.requesterId)
-	group, err := gc.groupRepo.GetGroupById(gc.Req.groupId)
-	approver, err := gc.userRepo.GetUserById(gc.Req.approverId)
+	requester, err := gc.userRepo.GetUserById(gc.req.requesterId)
+	group, err := gc.groupRepo.GetGroupById(gc.req.groupId)
+	approver, err := gc.userRepo.GetUserById(gc.req.approverId)
 	if err != nil {
-		gc.Res.Err = err
+		gc.res.Err = err
 		return
 	}
 	if !slices.Contains(group.Admins, approver.ID) && approver != group.Owner {
 		errMsg := "permission denied"
-		gc.Res.Err = errors.New(errMsg)
+		gc.res.Err = errors.New(errMsg)
 		return
 	}
 
 	idx := slices.IndexFunc(group.JoinRequests, func(req *entity.JoinRequest) bool {
-		return req.Group == gc.Req.groupId && req.Requester == gc.Req.requesterId
+		return req.Group == gc.req.groupId && req.Requester == gc.req.requesterId
 	})
 
 	if idx < 0 {
 		errMsg := "request not found"
-		gc.Res.Err = errors.New(errMsg)
+		gc.res.Err = errors.New(errMsg)
 		return
 	}
 
-	if gc.Req.action == ACCEPT_JOIN_REQUEST {
+	if gc.req.action == ACCEPT_JOIN_REQUEST {
 		group.AddMembers(requester.ID)
 	}
 
 	group.JoinRequests = slices.Delete(group.JoinRequests, idx, idx+1)
 
 	gc.groupRepo.Save(group)
-	gc.Res.Err = nil
+	gc.res.Err = nil
 }
 
 func NewHandleJoinRequestUseCase(
